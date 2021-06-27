38fc1f59e29ada143066d890ff5827f4c9d95d6d
Viktor Kolosov <vitykolosov@gmail.com>
2021-06-27 14:53:36 +0300

    feat: implement basic functionality

--- 04-mini-game/script.js
+++ 04-mini-game/script.js
@@ -1,16 +1,37 @@
 const board = document.getElementById('board');
 const numberOfSquares = 144;
+const colors = ['red', 'green', 'yellow', 'purple'];
 
 function fillBoard () {
   for (let i = 0; i < numberOfSquares; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+
+    square.addEventListener('mouseover', () => {
+      setColor(square);
+    });
+    square.addEventListener('mouseleave', () => {
+      removeColor(square);
+    });
+
     board.append(square);
   }
 }
 
-board.addEventListener('mouseover', (e) => {
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `${color} 0 0 2px 1px, 0 0 10px ${color}`
+}
 
-});
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1f1f1f';
+  element.style.boxShadow = '#1f1f1f 0 0 4px 2px';
+}
 
 fillBoard();
